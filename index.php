ff4d6aa88547af528d28ee800b55dd27041c1c61
Arturo Martinez <nsi.soluciones@gmail.com>
2016-12-18 17:31:41 -0500

    Seguridad. Ocultar claves en capa adicional-debug- coma en final json

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 session_start();
 include "bin/config.php";
 require_once __DIR__ . '/vendor/autoload.php';
-
+$accesso= new Config();
+$aid= $accesso->get_id();
+$asec=$accesso->get_secret();
+$aver=$accesso->get_version();
 $fb = new Facebook\Facebook([
-  'app_id' => $accesso->get_id(),
-  'app_secret' => $accesso->get_secret(),
-  'default_graph_version' => $accesso->get_version(),
+  'app_id' => $aid,
+  'app_secret' => $asec,
+  'default_graph_version' => $aver
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
--- login/procesaLogin.php
+++ login/procesaLogin.php
@@ -6,7 +6,7 @@ $accesso= new Config();
 $fb = new Facebook\Facebook([
   'app_id' => $accesso->get_id(),
   'app_secret' => $accesso->get_secret(),
-  'default_graph_version' => $accesso->get_version(),
+  'default_graph_version' => $accesso->get_version()
 ]);
 
   $helper = $fb->getRedirectLoginHelper();
