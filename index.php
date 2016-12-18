d25e270bf2ffb9ed8ff3734dcc1a36f014a4522f
Arturo Martinez <nsi.soluciones@gmail.com>
2016-12-18 17:33:01 -0500

    Seguridad. debug - test

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ $accesso= new Config();
 $aid= $accesso->get_id();
 $asec=$accesso->get_secret();
 $aver=$accesso->get_version();
-$fb = new Facebook\Facebook([
+echo $aid;
+echo $asec;
+echo $aver;
+/*$fb = new Facebook\Facebook([
   'app_id' => $aid,
   'app_secret' => $asec,
   'default_graph_version' => $aver
@@ -16,7 +19,7 @@ $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email', 'user_likes']; // optional
 $loginUrl = $helper->getLoginUrl('https://www.nslatino.com/nscomment/login/procesaLogin.php', $permissions);
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-
+*/
 
 
 
