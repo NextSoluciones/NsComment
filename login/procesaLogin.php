548fced36e86484e265919b9e1aaf48930cc05c4
Arturo Martinez <nsi.soluciones@gmail.com>
2016-12-18 21:54:03 -0500

    user node-2-exp-3

--- login/procesaLogin.php
+++ login/procesaLogin.php
@@ -4,12 +4,12 @@ include "./../bin/config.php";
 require_once __DIR__ . './../vendor/autoload.php';
 $accesso= new Config();
 var_dump($accesso);
-/*$fb = new Facebook\Facebook([
+$fb = new Facebook\Facebook([
   'app_id' => $accesso->get_id(),
   'app_secret' => $accesso->get_secret(),
   'default_graph_version' => $accesso->get_version()
 ]);
-
+/*
   $helper = $fb->getCanvasHelper();
   try {
     $accessToken = $helper->getAccessToken();
