d19b6a38bc48e5bb413212ca693da30b573dcb9b
Arturo Martinez <nsi.soluciones@gmail.com>
2016-12-18 21:53:12 -0500

    user node-2-exp-3

--- login/procesaLogin.php
+++ login/procesaLogin.php
@@ -3,8 +3,8 @@ session_start();
 include "./../bin/config.php";
 require_once __DIR__ . './../vendor/autoload.php';
 $accesso= new Config();
-
-$fb = new Facebook\Facebook([
+var_dump($accesso);
+/*$fb = new Facebook\Facebook([
   'app_id' => $accesso->get_id(),
   'app_secret' => $accesso->get_secret(),
   'default_graph_version' => $accesso->get_version()
@@ -41,6 +41,6 @@ $fb = new Facebook\Facebook([
     //header('Location: https://www.nslatino.com/nscomment/mod/me.php');
     // Now you can redirect to another page and use the
     // access token from $_SESSION['facebook_access_token']
-  }
+  }*/
 
 ?>
