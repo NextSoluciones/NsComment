e56ab5448803220cf3b4596fcbac3b4fbd0b1580
Arturo Martinez <nsi.soluciones@gmail.com>
2016-12-18 21:49:16 -0500

    user node-2-exp-2

--- login/procesaLogin.php
+++ login/procesaLogin.php
@@ -10,24 +10,35 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => $accesso->get_version()
 ]);
 
-  $helper = $fb->getRedirectLoginHelper();
-    try {
-      $accessToken = $helper->getAccessToken();
-    } catch(Facebook\Exceptions\FacebookResponseException $e) {
-      // When Graph returns an error
-      echo 'Graph returned an error: ' . $e->getMessage();
-      exit;
-    } catch(Facebook\Exceptions\FacebookSDKException $e) {
-      // When validation fails or other local issues
-      echo 'Facebook SDK returned an error: ' . $e->getMessage();
-      exit;
+  $helper = $fb->getCanvasHelper();
+  try {
+    $accessToken = $helper->getAccessToken();
   }
+  catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+  }
+
 
   if (isset($accessToken)) {
     // Logged in!
     $_SESSION['facebook_access_token'] = (string) $accessToken;
+
+
+    // Logged in
+    echo '<h3>Signed Request</h3>';
+    var_dump($helper->getSignedRequest());
+
+    echo '<h3>Access Token</h3>';
+    var_dump($accessToken->getValue());
+
     //echo "Login Exitoso. El access Token es ".$_SESSION['facebook_access_token'];
-    header('Location: https://www.nslatino.com/nscomment/mod/me.php');
+    //header('Location: https://www.nslatino.com/nscomment/mod/me.php');
     // Now you can redirect to another page and use the
     // access token from $_SESSION['facebook_access_token']
   }
