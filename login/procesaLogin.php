629ffd3d1ece1be5f264693235f7b94b41a15a42
Arturo Martinez <nsi.soluciones@gmail.com>
2016-12-18 22:01:39 -0500

    user node-2-exp-3-z

--- login/procesaLogin.php
+++ login/procesaLogin.php
@@ -3,45 +3,33 @@ session_start();
 include "./../bin/config.php";
 require_once __DIR__ . './../vendor/autoload.php';
 $accesso= new Config();
-var_dump($accesso);
+
 $fb = new Facebook\Facebook([
   'app_id' => $accesso->get_id(),
   'app_secret' => $accesso->get_secret(),
   'default_graph_version' => $accesso->get_version()
 ]);
 
-  $helper = $fb->getCanvasHelper();
-  try {
-    $accessToken = $helper->getAccessToken();
-  } catch(Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-  } catch(Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
+  $helper = $fb->getRedirectLoginHelper();
+    try {
+      $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+      // When Graph returns an error
+      echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+      // When validation fails or other local issues
+      echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
   }
 
-  if (! isset($accessToken)) {
-    echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
-    exit;
-  }
-
-    //$_SESSION['facebook_access_token'] = (string) $accessToken;
-
-
-    // Logged in
-    echo '<h3>Signed Request</h3>';
-    //var_dump($helper->getSignedRequest());
-
-    echo '<h3>Access Token</h3>';
-    //var_dump($accessToken->getValue());
-
+  if (isset($accessToken)) {
+    // Logged in!
+    $_SESSION['facebook_access_token'] = (string) $accessToken;
     //echo "Login Exitoso. El access Token es ".$_SESSION['facebook_access_token'];
-    //header('Location: https://www.nslatino.com/nscomment/mod/me.php');
+    header('Location: https://www.nslatino.com/nscomment/mod/me.php');
     // Now you can redirect to another page and use the
     // access token from $_SESSION['facebook_access_token']
-  */
+  }
 
 ?>
--- mod/me.php
+++ mod/me.php
@@ -11,6 +11,24 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => $accesso->get_version()
 ]);
 
+$helper = $fb->getCanvasHelper();
+try {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+if (! isset($accessToken)) {
+  echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
+  exit;
+}
+
 // Sets the default fallback access token so we don't have to pass it to each request
 $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 try {
