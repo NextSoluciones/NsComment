6b4de6e0603e2718fbae1a77730b101455c024a0
Arturo Martinez <ventas@nslatino.com>
2016-12-29 11:25:58 -0500

    formularios+span+debug

--- mod/me.php
+++ mod/me.php
@@ -48,6 +48,9 @@ $fb = new Facebook\Facebook([
   <article class="busqueda">
     <form class="formulario" action="procesaPost.php" method="post">
       <span>Post a procesar (id.): </span><input type="text" name="id_post" value="" placeholder="p.e. 10548577854452"/>
+      <input type="hidden" name="id" value="<?php
+          echo $userNode->getName();
+      ?>">
       <input type="submit" value="Procesar">
     </form>
   </article>
--- mod/procesaPost.php
+++ mod/procesaPost.php
@@ -10,6 +10,21 @@ $fb2 = new Facebook\Facebook([
   'default_graph_version' => $accesso->get_version()
 ]);
 
+$fb2->setDefaultAccessToken($_SESSION['facebook_access_token']);
+try {
+  $response = $fb2->get('/me');
+  $userNode = $response->getGraphUser();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
 $post=$_POST['id_post'];
-echo $post;
+$id=$_POST['id'];
+echo $id."_".$post;
 ?>
