88584bbcbee230716725ca57e0c8aa908747d798
Arturo Martinez <ventas@nslatino.com>
2016-12-29 10:51:31 -0500

    base 1

--- mod/me.php
+++ mod/me.php
@@ -42,22 +42,7 @@ $fb = new Facebook\Facebook([
 
     echo 'Haz iniciado sesión correctamente como ' . $userNode->getName();
     $_SESSION['logueado'] =true;
-    /*echo "<br/>";
-    var_dump($userNode);
-    $plainOldArray = $response->getDecodedBody();
-    var_dump($plainOldArray);*/
-
-    $request = new FacebookRequest(
-      $session,
-      'GET',
-      '/me/friends'
-    );
-
-    $response = $request->execute();
-    $graphObject = $response->getGraphObject();
-    /* handle the result */
-    var_dump($graphObject);
-
+    
 ?>
 <footer><ul id="pie">
   <li><a href="./../legal.html">Avisos Legales y Pol&iacute;tica de Privacidad</a></li>
