d085f000fbbc53383bc46c5f34879127ae202a26
Arturo Martinez <ventas@nslatino.com>
2016-12-29 11:16:03 -0500

    formularios+span+debug

--- mod/procesaPost.php
+++ mod/procesaPost.php
@@ -2,7 +2,9 @@
 session_start();
 include "./../bin/config.php";
 require_once __DIR__ . './../vendor/autoload.php';
-$fb = new Facebook();
+$fb = new Facebook('app_id' => '',
+'app_secret' => '',
+'default_graph_version' => '');
 
 $post=$_POST['id_post'];
 echo $post;
