15ae02d84adc71b1356abbe52fe394e2a88b25fe
Arturo Martinez <ventas@nslatino.com>
2016-12-30 11:42:31 -0500

    Carga comentarios y subcomentarios

--- mod/procesaPost.php
+++ mod/procesaPost.php
@@ -15,24 +15,40 @@ $id=$_POST['id'];
 $recurso=$id."_".$post;
 $res = [];
 $fb2->setDefaultAccessToken($_SESSION['facebook_access_token']);
-try {
-  $response = $fb2->get('/'.$recurso.'/comments?fields=from,message,comment_count');
-  $postNode = $response->getGraphEdge();
-  foreach ($postNode as $nodo) {
-      $vector=$nodo->asArray();
-      $sub=$vector["comment_count"];
-      echo $sub;
-      $res[]=$vector;
+
+$flag=1;
+$iterator=0;
+$cola = [];
+$cola[0]=$recurso;
+
+while ($flag > 0) {
+  try {
+    $response = $fb2->get('/'.$cola[$iterator].'/comments?fields=from,message,comment_count');
+    $postNode = $response->getGraphEdge();
+    foreach ($postNode as $nodo) {
+        $vector=$nodo->asArray();
+        $sub=$vector["comment_count"];
+        if ($sub > 0) {
+          $iterator++;
+          $flag++;
+          $cola[$iterator]=$vector["id"];
+        }
+        $res[]=$vector;
+        $flag--;
+    }
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    $flag--;
+    exit;
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    $flag--;
+    exit;
   }
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
 }
+
 //var_dump($res);
 foreach ($res as $item) {
   echo $item["from"]["name"]." : ".$item["message"]." >> ".$item["comment_count"]."<br/>";
