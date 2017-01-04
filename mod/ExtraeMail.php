83a10bd9161d52c57cc5b6889fc774898a21dc62
Arturo Martinez <ventas@nslatino.com>
2017-01-03 22:15:37 -0500

    se cuelga

--- mod/ExtraeMail.php
+++ mod/ExtraeMail.php
@@ -75,11 +75,15 @@ class ExtraeMail{
         $this->debug.="<br/>temp2=".$temp2."<br/>";
         $this->debug.="<br/>start=".$start."<br/>";
         $this->debug.="<br/>iterator-pre=".$iterator."<br/>";
-        if (filter_var($temp2, FILTER_VALIDATE_EMAIL)) {
-          $usuario=$temp;
-          $iterator++;
-          $this->debug.="<br/>iterator-post=".$iterator."<br/>";
-        }
+        try {
+          if (filter_var($temp2, FILTER_VALIDATE_EMAIL)) {
+            $usuario=$temp;
+            $iterator++;
+            $this->debug.="<br/>iterator-post=".$iterator."<br/>";
+          }
+        } catch (Exception $e) {
+          $this->debug.="<br/>Exception: ".$e."<br/>";
+        } 
         $iterator--;
       }        //punto de control --error
       /*
