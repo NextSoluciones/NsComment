43ab94444d46866e5f88d221dccd76145d0153d0
Arturo Martinez <ventas@nslatino.com>
2017-01-03 21:41:10 -0500

    debug - agregado pruebas

--- pruebas/main.php
+++ pruebas/main.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include "./ExtraeMail.php";
   $cad="Hola a todos les paso mi correo:ajms@yahoo.com.es pero.pe su pronta respuesta. Tambien me pueden escribir al:2012100594@ucss.pe.gracias";
   $data=new ExtraeMail();
