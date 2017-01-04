cb27f8a0654fd74d71ed5d103c32af37a2a3682c
Arturo Martinez <ventas@nslatino.com>
2017-01-04 07:34:20 -0500

    se cuelga

--- mod/ExtraeMail.php
+++ mod/ExtraeMail.php
@@ -69,17 +69,39 @@ class ExtraeMail{
       $user=$sub[0];
       $control=0;
       #Parece que entra en un bucle infinito. Poner un control que limite a 10
+      $this->debug="<br/>iterator inicial: ".$iterator."<br/>";
+      $this->debug="<br/>start inicial: ".$start."<br/>";
+      $this->debug="<br/>user inicial: ".$user."<br/>";
+      $this->debug="<br/>control inicial: ".$control."<br/>";
       while ($iterator > 0&& $control<10) {
+        $this->debug="<br/>***********NUEVO CICLO************<br/>";
+        $this->debug="<br/>iterator: ".$iterator."<br/>";
+        $this->debug="<br/>start: ".$start."<br/>";
+        $this->debug="<br/>user: ".$user."<br/>";
+        $this->debug="<br/>control: ".$control."<br/>";
           $control++;
           $temp=substr($user,$start);
+          $this->debug="<br/>temp: ".$temp."<br/>";
           $temp2=$temp."@dominio.tld";
+          $this->debug="<br/>temp2: ".$temp2."<br/>";
           $start--;
             if (filter_var($temp2, FILTER_VALIDATE_EMAIL)) {
+              $this->debug="<br/>******CONDICIONAL FILTRO*********<br/>";
               $usuario=$temp;
+              $this->debug="<br/>user: ".$user."<br/>";
               $iterator++;
+              $this->debug="<br/>iterator: ".$iterator."<br/>";
             }
           $iterator--;
+          $this->debug="<br/>******DECRECE ITERATOR*********<br/>";
+          $this->debug="<br/>iterator: ".$iterator."<br/>";
+          $this->debug="<br/>******FIN CICLO*********<br/>";
         }
+        $this->debug="<br/>******ESTADO FINAL WHILE*********<br/>";
+        $this->debug="<br/>iterator final: ".$iterator."<br/>";
+        $this->debug="<br/>start final: ".$start."<br/>";
+        $this->debug="<br/>user final: ".$user."<br/>";
+        $this->debug="<br/>control final: ".$control."<br/>";
       //punto de control --error
 
       $correoFinal=$usuario."@".$host[0].".".$this->tld1;
