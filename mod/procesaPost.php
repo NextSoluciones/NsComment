baf07cec88d9a72f186fa24dd18702b55378441a
Arturo Martinez <ventas@nslatino.com>
2017-01-03 22:29:13 -0500

    se cuelga

--- mod/procesaPost.php
+++ mod/procesaPost.php
@@ -1,8 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+ini_set('memory_limit', '256M');
 session_start();
-
 include "./../bin/config.php";
 require_once "ExtraeMail.php";
 require_once __DIR__ . './../vendor/autoload.php';
