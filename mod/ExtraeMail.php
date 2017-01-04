eb0e685fd8a39712876817ad1eb3e33df9fdca4f
Arturo Martinez <ventas@nslatino.com>
2017-01-03 22:04:27 -0500

    se cuelga

--- mod/ExtraeMail.php
+++ mod/ExtraeMail.php
@@ -67,7 +67,7 @@ class ExtraeMail{
       $iterator=1;
       $start=-1;
       $user=$sub[0];
-      /*while ($iterator > 0) {
+      while ($iterator > 0) {
         $temp=substr($user,$start);
         $temp2=$temp."@dominio.tld";
         $start--;
@@ -76,7 +76,7 @@ class ExtraeMail{
           $iterator++;
         }
         $iterator--;
-      }*/         //punto de control --error
+      }        //punto de control --error
       /*
       $correoFinal=$usuario."@".$host[0].".".$this->tld1;
       if ($this->bool_tld2) {
