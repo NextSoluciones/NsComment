9383d443af9cf7938b07ab4facd197a3f0f2239d
Arturo Martinez <ventas@nslatino.com>
2017-01-13 22:00:53 -0500

    corregido, detecta cuando hay caracteres incorrectos al inicio y completa cuando no se coloca el .com pero existe un @xxx

--- mod/procesaPost.php
+++ mod/procesaPost.php
@@ -91,7 +91,7 @@ foreach ($res as $item) {
 }
 echo "</tbody></table></div>";
 echo "</form>";
-echo "Debugg: ".$data->debug();
+//echo "Debugg: ".$data->debug();
 echo "<div class='fin'></div>";
 ?>
 <footer><ul id="pie">
