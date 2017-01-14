7c3669b437a7ad0d76c74fc7d0c02ccc90a51039
Arturo Martinez <ventas@nslatino.com>
2017-01-13 20:13:28 -0500

    error cuando la seccion de un email empieza con un caracter inválido luego de un espacio

--- mod/ExtraeMail.php
+++ mod/ExtraeMail.php
@@ -71,44 +71,28 @@ class ExtraeMail{
       $user=$sub[0];
       $control=0;
       $nuser=strlen($user);
-      #Parece que entra en un bucle infinito. Poner un control que limite a 10
-    //  $this->debug.="<br/>iterator inicial: ".$iterator."<br/>";
-      //$this->debug.="<br/>start inicial: ".$start."<br/>";
-    //  $this->debug.="<br/>user inicial: ".$user."<br/>";
-      //$this->debug.="<br/>control inicial: ".$control."<br/>";
-      while ($iterator > 0&& $control<1000) {
-      //  $this->debug.="<br/>***********NUEVO CICLO************<br/>";
-      //  $this->debug.="<br/>iterator: ".$iterator."<br/>";
-      //  $this->debug.="<br/>start: ".$start."<br/>";
-        //$this->debug.="<br/>user: ".$user."<br/>";
-        //$this->debug.="<br/>control: ".$control."<br/>";
+
+      while ($iterator > 0&& $control<1500) {
           $control++;
           $temp=substr($user,$start);
-        //  $this->debug.="<br/>temp: ".$temp."<br/>";
           $temp2=$temp."@dominio.tld";
-        //  $this->debug.="<br/>temp2: ".$temp2."<br/>";
           $start++;
             if (filter_var($temp2, FILTER_VALIDATE_EMAIL)) {
-        //      $this->debug.="<br/>******CONDICIONAL FILTRO*********<br/>";
               $usuario=$temp;
-        //      $this->debug.="<br/>user: ".$user."<br/>";
               $iterator--;
-        //      $this->debug.="<br/>iterator: ".$iterator."<br/>";
               if (($nuser+1)==abs($start)) {
-                $iterator++;
+                $iterator--;
               }
             }
-          $iterator--;
-        //  $this->debug.="<br/>******DECRECE ITERATOR*********<br/>";
-        //  $this->debug.="<br/>iterator: ".$iterator."<br/>";
-        //  $this->debug.="<br/>******FIN CICLO*********<br/>";
+            else {
+              if (abs($start)<($nuser+1)) {
+                $iterator++;
+              }
+              else {
+                $iterator--;
+              }
+            }
         }
-        //$this->debug.="<br/>******ESTADO FINAL WHILE*********<br/>";
-        //$this->debug.="<br/>iterator final: ".$iterator."<br/>";
-        //$this->debug.="<br/>start final: ".$start."<br/>";
-        //$this->debug.="<br/>user final: ".$user."<br/>";
-        //$this->debug.="<br/>control final: ".$control."<br/>";
-      //punto de control --error
 
       $correoFinal=$usuario."@".$host[0].".".$this->tld1;
       if ($this->bool_tld2) {
